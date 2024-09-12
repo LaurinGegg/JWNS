149d16ce2b9e9d5dbbdd4d911a7d69fa198c254d
LaurinGegg <lauringegg@web.de>
2024-09-12 14:42:28 +0200

    Rechnungen

--- JWNS/src/Vertex.java
+++ JWNS/src/Vertex.java
@@ -4,20 +4,26 @@ class Vertex {
     public double x;
     public double y;
     public static int i;
+    public static int j;
 
     public Vertex(double x, double y) {
         this.x = x;
         this.y = y;
-
+        j=999999999;
     }
 
     public static void main(String[] args) {
         Vertex v1 = new Vertex(42, 17);
         Vertex v2 = new Vertex(0.3, 24.6);
-        for (i = 0; i < 10; i++) {
+        System.out.println(v1);
+        while (v1.x < j) {
+
             v1.x = v1.x + v2.x;
             v1.y = v1.y + v2.y;
-            System.out.println(v1);
+            if (v1.x <= j) {
+                System.out.println(v1);
+
+            }
         }
     }
 
