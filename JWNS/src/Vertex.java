33f47878a49ad70f9903cd0b72b85b874d16eafb
LaurinGegg <lauringegg@web.de>
2024-09-17 09:03:10 +0200

    17.09.

--- JWNS/src/Vertex.java
+++ JWNS/src/Vertex.java
@@ -12,19 +12,7 @@ class Vertex {
         j = 999999999;
     }
 
-    public static void main(String[] args) {
-        Vertex v1 = new Vertex(42, 17);
-        Vertex v2 = new Vertex(0.3, 24.6);
-        Vertex v3 = v1.skalarMult(2);
-
-        System.out.println(v1);
-        System.out.println(v3);
-        System.out.println(v1.length());
-        System.out.println(v2.length());
-        System.out.println(v1.equals(v3));
-        System.out.println(v1.equals(v1));
-
-    }
+  
 
     public double length() {
         return Math.sqrt(x * x + y * y);
