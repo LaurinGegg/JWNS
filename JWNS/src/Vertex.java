83a18005eb86c3f2bdf2897e51f1cdb4217e8390
LaurinGegg <lauringegg@web.de>
2024-09-17 07:31:04 +0200

    Informatik

--- JWNS/src/Vertex.java
+++ JWNS/src/Vertex.java
@@ -15,8 +15,10 @@ class Vertex {
     public static void main(String[] args) {
         Vertex v1 = new Vertex(42, 17);
         Vertex v2 = new Vertex(0.3, 24.6);
+        Vertex v3 = v1.skalarMult(2);
 
         System.out.println(v1);
+        System.out.println(v3);
         System.out.println(v1.length());
         System.out.println(v2.length());
 
@@ -29,6 +31,11 @@ class Vertex {
 
     public Vertex skalarMult(double s) {
         return new Vertex(x * s, y * s);
+    }
+
+    public void skalarMultMod(double s) {
+        x = x * s;
+        y = y * s;
     }
 
     public String toString() {
