349bf0a11855f3c657f7b0182da925d3749aabbd
LaurinGegg <lauringegg@web.de>
2024-10-08 08:09:10 +0200

    8.10

---
+++ JWNS/src/SizedPanel.java
@@ -0,0 +1,16 @@
+package JWNS.src;
+import javax.swing;
+import java.awt.Dimension;
+
+public class SizedPanel extends JPanel{
+int preferredWidth = 300;
+int preferredHeight = 400;
+
+public SizedPanel(int w, int h){
+    preferredWidth = w;
+    preferredHeight = h;
+}
+}
+ 
+    
+
